6c14755288ad5f34027c64c8e167dd2ab4333b2f
Maimonides-CS <91688173+Maimonides-CS@users.noreply.github.com>
2022-01-24 09:37:42 -0600

    Create I_plus_plus.java

---
+++ I_plus_plus.java
@@ -0,0 +1,12 @@
+public class I_plus_plus {
+  public static void main(String[] args) {
+   // int i= Integer.MAX_VALUE;
+   // int j = Integer.MIN_VALUE;
+   // System.out.println(i);
+   // System.out.println(j);
+    //int c = 10, d=4;
+    int i=3;
+    System.out.println(i++);
+    System.out.println(++i);
+  }
+}
