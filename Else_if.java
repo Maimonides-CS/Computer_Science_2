3be5c005612d8ba47ee88d454da7ed29b3991cb5
Maimonides-CS <91688173+Maimonides-CS@users.noreply.github.com>
2022-01-24 10:01:30 -0600

    Create Else_if.java

---
+++ Else_if.java
@@ -0,0 +1,15 @@
+// Import the HashMap class
+import java.util.HashMap;
+import java.io.*;
+
+public class Else_if {
+  public static void main(String[] args) {
+    int n = 9; 
+    if (n > 0) {
+        if (n % 2 == 0)
+           System.out.println(n);
+          }
+    else 
+        System.out.println(n + " is not positive");    
+  }
+}
