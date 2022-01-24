8552f93381619247b1cb812a46e3faa8b59cc87e
Maimonides-CS <91688173+Maimonides-CS@users.noreply.github.com>
2022-01-24 10:13:17 -0600

    Create For_loop.java

---
+++ For_loop.java
@@ -0,0 +1,15 @@
+// Import the HashMap class
+import java.util.HashMap;
+import java.io.*;
+
+public class For_loop {
+  public static void main(String[] args) {
+    int i;
+    for (i = 1; i < 5; i++) {
+       System.out.println(i + " ");
+      }
+    
+    System.out.println("i:" + i);
+    
+  }
+}
