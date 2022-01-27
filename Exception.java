ccb439551183007f1e6342d175b364b3f93dd55d
Maimonides-CS <91688173+Maimonides-CS@users.noreply.github.com>
2022-01-27 09:08:59 -0600

    Update Exception.java

--- Exception.java
+++ Exception.java
@@ -1,20 +1,19 @@
+// Import the HashMap class
+import java.util.HashMap;
 import java.io.*;
-import java.util.*;
 
 public class Exception {
   public static void main(String[] args) {
-    setRadius(9);
+    setRadius(-9);
+  }
 
-   }
-   public static void setRadius(int newRadius)
-   {
-   if (newRadius < 0)
-   throw new IllegalArgumentException
-   ("Radius cannot be negative");
-   else
-   radius = newRadius;
-   
-   }
-
-
+  public static void setRadius(int newRadius) {
+    //int radius;
+    if (newRadius < 0)
+    throw new IllegalArgumentException
+      ("Radius cannot be negative");
+    else
+       System.out.println(newRadius);
+      //radius = newRadius;
+    }    
 }
