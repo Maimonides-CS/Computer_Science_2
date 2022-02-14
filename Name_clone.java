602638a0eb741b4f157d42d57a3cc2ffd590a7b8
Maimonides-CS <91688173+Maimonides-CS@users.noreply.github.com>
2022-02-14 09:57:35 -0600

    Update Name_clone.java

--- Name_clone.java
+++ Name_clone.java
@@ -5,16 +5,18 @@ public class Name_clone implements Cloneable{
   // Call the constructor
   private int x;
   private String y;
+  public String[] z=new String[4];
+
   public Name_clone(int x, String y) {
     this.x = x;  // Set the initial value for the class attribute x
     this.y = y;
   }
 
-  public boolean equals(Name_clone name3)
+  public String toString()
   {
-    if (this.x!=name3.x) return false;
-    return true;
+    return "(" + this.x + ", " + this.y + ")";
   }
+
 
   public Object clone() throws CloneNotSupportedException
   {
@@ -25,7 +27,6 @@ public class Name_clone implements Cloneable{
     Name_clone name1= new Name_clone(1, "hello");
     Name_clone name2= new Name_clone(2, "hello");
     Name_clone name4 = (Name_clone) name2.clone();
-    System.out.println(name1.equals(name2));
+    System.out.println(name4);
   }
 }
-
