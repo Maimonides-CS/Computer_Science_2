ad5eb77ca88ae7f856ebed70cf1fcfcb43fab1ed
Maimonides-CS <91688173+Maimonides-CS@users.noreply.github.com>
2022-05-09 11:29:49 -0400

    Create MergeSort.java

---
+++ MergeSort.java
@@ -0,0 +1,22 @@
+public class MergeSort {
+
+    public static void main(String[] args) {
+        int[] input = new int[]{3, 2, 1, 0, 6, 7, 4, 5};
+
+       
+    }
+
+    public void Combine(int low, int middle, int high) {
+
+
+    }
+
+    public void divide (int low, int high) {
+        //if ((high-low) == 1)
+        int mid = (high+low)/2;
+        divide(low, mid);
+        divide();
+    }
+
+
+}
