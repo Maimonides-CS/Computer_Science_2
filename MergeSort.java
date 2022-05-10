e8cb2bfa8c60fc139d9e797a9b9c49a439d0e0dd
Maimonides-CS <91688173+Maimonides-CS@users.noreply.github.com>
2022-05-10 09:40:51 -0400

    Update MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -1,21 +1,57 @@
 public class MergeSort {
-
+    private int[] input = new int[]{3, 2, 1, 0, 6, 7, 4, 5};
+    private int[] temp = new int[8];
     public static void main(String[] args) {
-        int[] input = new int[]{3, 2, 1, 0, 6, 7, 4, 5};
+        
 
        
     }
 
     public void Combine(int low, int middle, int high) {
+      
+      for (int i = low; i<= high; i++) {
+        temp[i] = input[i];
+      }
 
+      // int i: index of the left subarray 
+      // int j: index of the right subarray
+      // int z: index of the combined array
+      int i = low, j = middle +1, z = low;
+      //z <= high
+      while ((i <= middle)&&(j <= high)) {
+
+          if (this.temp[i] < this.temp[j]) {
+            input[z] = temp[i];
+             i++;
+          }
+          else {
+            input[z] = temp[j];
+            j++;
+          }
+          z++;
+      }
+      while (i <= middle) {
+        input[z] = temp[i];
+        i++;
+        z++;
+      }
+
+      while (j <= high) {
+        input[z] = temp[j];
+        j++;
+        z++;
+      }
 
     }
 
     public void divide (int low, int high) {
-        //if ((high-low) == 1)
+        if ((high-low) == 1) {
+            Combine();
+        }
         int mid = (high+low)/2;
         divide(low, mid);
-        divide();
+        divide(mid+1, high);
+
     }
 
 
