27fd76e3e42c6732b84b1f2aaa047de64929ebb1
Maimonides-CS <91688173+Maimonides-CS@users.noreply.github.com>
2022-05-26 10:10:55 -0400

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -1,3 +1,5 @@
+import java.util.*; 
+
 /*
 [1, 3, 2, 7, 4, 5, 6]
  [1, 3, 2, 4, 7, 5, 6]
@@ -8,20 +10,29 @@
  */
 
 public class QuickSort {
-    private static int[] AA = {1, 3, 2, 7, 4, 5, 6};
- 
+    private static int[] AA = {1, 7, 7};
+    //private static int[] AA = {2, 1};
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(AA));
 
         quicksort(0, AA.length-1);
- 
+        
+        System.out.println(Arrays.toString(AA));
     }    
 
     public static void  quicksort(int low, int high) {
-        if (low<high) {
+        System.out.println("low: " +low + " high: " + high);
+
+       
             int index = partition(low, high);
-            quicksort(low, index);
-            quicksort(index+1, high);
-        }
+            System.out.println("index: " + index);
+            if (low < index-1) {
+            quicksort(low, index-1);
+            }
+            if (index <high) {
+            quicksort(index, high);
+            }
+        
 
     }
 
@@ -29,11 +40,14 @@ public class QuickSort {
 
  
     public static int partition (int left, int right) {
+        System.out.println("left: " + left + " right: " + right);
+
         int mid = (left + right)/2;
- 
+        System.out.println("mid: " + mid);
+
         int i = left, j = right;
  
-        while (i < j) {
+        while (i <= j) {
 
              while ( AA[mid]> AA[i]) {
                  i++;
@@ -42,7 +56,7 @@ public class QuickSort {
                  j--;
              }
           
-            if (i<j) {
+            if (i<=j) {
               int temp = AA[i];
               AA[i] = AA[j];
               AA[j] = temp;
@@ -51,7 +65,8 @@ public class QuickSort {
             }
  
         }
-
+        
+        System.out.println("i: " + i);
         return i;
        
  
